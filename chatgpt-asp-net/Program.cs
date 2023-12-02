64743e4d4bd4183ebca3e019b865098d79738f3e
Mario Camelo <mariuo@gmail.com>
2023-12-02 10:57:10 -0500

    Project integration with ChatGPT OPenAI

---
+++ chatgpt-asp-net/Program.cs
@@ -0,0 +1,40 @@
+using ChatGPT.ASPNET.Extensions;
+
+var appName = "ChatGPT ASP.NET";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddSerilog(builder.Configuration, appName);
+builder.AddChatGpt(/*builder.Configuration*/);
+
+// Add services to the container.
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddControllers();
+
+builder.Services.AddSwagger(builder.Configuration, appName);
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+/*
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}*/
+
+//app.AddSwaggerDoc();
+
+app.UseSwaggerDoc(appName);
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
